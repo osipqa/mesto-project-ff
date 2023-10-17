7f20f0cbc68d88b8b191d1eff608f39874191e8f
Sergey Osipov <sery.osipow@yandex.ru>
2023-10-17 16:40:58 +0300

    check work

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,20 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+
+function createCard(data) {
+  const templateCard = document.querySelector('#card-template').content;
+  const card = templateCard.cloneNode(true);
+  card.querySelector('.card__image').src = data.link;
+  card.querySelector('.card__title').textContent = data.name;
+  card.querySelector('.card__delete-button').addEventListener('click', evt => {const card = evt.target.closest('.card'); card.remove();});
+  return card;
+}
+
+function releaseCard() {
+  initialCards.forEach(card => cardList.append(createCard(card)));
+}
+
+releaseCard();
