a09f39508354305ba2bd06107d67395480195be7
Sergey Osipov <sery.osipow@yandex.ru>
2023-10-17 18:35:13 +0300

    fix after review

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,23 @@
 
 const cardList = document.querySelector('.places__list');
 
-function createCard(data) {
+function createCard(data, deleteCard) {
   const templateCard = document.querySelector('#card-template').content;
   const card = templateCard.cloneNode(true);
   card.querySelector('.card__image').src = data.link;
+  card.querySelector('.card__image').alt = data.name;
   card.querySelector('.card__title').textContent = data.name;
-  card.querySelector('.card__delete-button').addEventListener('click', evt => {const card = evt.target.closest('.card'); card.remove();});
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
   return card;
 }
 
+function deleteCard(evt) {
+  const card = evt.target.closest('.card');
+  card.remove();
+}
+
 function releaseCard() {
-  initialCards.forEach(card => cardList.append(createCard(card)));
+  initialCards.forEach(card => cardList.append(createCard(card, deleteCard)));
 }
 
 releaseCard();
